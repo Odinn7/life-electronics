6f5033f52e8ce0e020379381d86a722e50fc3224
Andrew <andreyod@wizardsdev.com>
2021-12-22 15:09:25 +0400

    123

--- src/components/partners/styled.js
+++ src/components/partners/styled.js
@@ -1,12 +1,4 @@
 import styled from "styled-components";
-import Bill from "../../assets/images/image1.png";
-// import Mark from "../../assets/images/image2.png";
-// import David from "../../assets/images/image3.png";
-// import John from "../../assets/images/image4.png";
-// import Gido from "../../assets/images/image5.png";
-// import Pavel from "../../assets/images/image6.png";
-// import Sergey from "../../assets/images/image7.png";
-// import Yevgen from "../../assets/images/image8.png";
 
 export const PartnersWrapper = styled.div`
   && {
